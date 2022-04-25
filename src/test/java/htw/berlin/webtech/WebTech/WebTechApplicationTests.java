50136678b19723cf06d4badec3d7959b7030be7a
nicomarantos <85935247+nicomarantos@users.noreply.github.com>
2022-04-25 21:36:17 +0200

    init

---
+++ src/test/java/htw/berlin/webtech/WebTech/WebTechApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.WebTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
