006a6b7f96ef3746275c75dbcee6d40c99c75176
nicomarantos <85935247+nicomarantos@users.noreply.github.com>
2022-05-13 13:18:21 +0200

    Entity added

---
+++ src/main/java/htw/berlin/webtech/WebTech/persistence/PlantEntity.java
@@ -0,0 +1,54 @@
+package htw.berlin.webtech.WebTech.persistence;
+
+import javax.persistence.*;
+
+@Entity(name = "plants")
+public class PlantEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "common_name", nullable = false)
+    private String commonName;
+    @Column(name = "botanical_name", nullable = false)
+    private String botanicalName;
+    @Column(name = "is_saved")
+    private Boolean saved;
+
+    public PlantEntity(Long id, String commonName, String botanicalName, Boolean saved) {
+        this.id = id;
+        this.commonName = commonName;
+        this.botanicalName = botanicalName;
+        this.saved = saved;
+    }
+
+    protected PlantEntity(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getBotanicalName() {
+        return botanicalName;
+    }
+
+    public void setBotanicalName(String botanicalName) {
+        this.botanicalName = botanicalName;
+    }
+
+    public Boolean getSaved() {
+        return saved;
+    }
+
+    public void setSaved(Boolean saved) {
+        this.saved = saved;
+    }
+}
